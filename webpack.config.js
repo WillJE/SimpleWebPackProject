dd40d4e74db00dfbb4b6c8de5b7e1afa4e5d9e90
WillJE <18170875354@163.com>
2017-10-20 13:28:47 +0800

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = {
 	devtool: 'eval-source-map',
 	entry: __dirname + "/app/main.js",
@@ -6,8 +8,44 @@ module.exports = {
 		filename: "bundle.js"
 	},
 	devServer: {
-    contentBase: "./public",
-    historyApiFallback: true,
-    inline: true
-	} 
-}
+		contentBase: "./public",
+		historyApiFallback: true,
+		inline: true
+	}, 
+	module: {
+		rules: [
+		{
+			test: /(\.jsx|\.js)$/,
+			use: {
+				loader: "babel-loader",
+				options: {
+					presets: [
+					"es2015", "react"
+					]
+				}
+			},
+			exclude: /node_modules/
+		},
+		{
+			test: /\.css$/,
+			use: [
+			{
+				loader: "style-loader"
+			},
+			{
+				loader: "css-loader",
+				options: {
+					modules: true
+				}
+			},
+			{
+				loader: "postcss-loader"
+			}
+			]
+		}
+		]
+	},
+	plugins: [
+    	new webpack.BannerPlugin('This file is created by zhaoda')
+  	]
+};
